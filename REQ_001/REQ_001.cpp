d3fd3c10d0e8459922d99157f6f4b1c5ee76e71c
Paul Santiago <70616143+prsanti@users.noreply.github.com>
2024-02-14 23:20:13 -0500

    add REQ_001 unit test files

---
+++ REQ_001/REQ_001.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" int getPerimeter(int, int);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace REQ001
+{
+	TEST_CLASS(REQ001)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
