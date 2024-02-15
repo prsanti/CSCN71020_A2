d1fe7e6d8c8def7fdd47257ccea75d5e7b7204f2
Paul Santiago <70616143+prsanti@users.noreply.github.com>
2024-02-14 23:33:57 -0500

    add test method for perimeter and area functions

--- REQ_001/REQ_001.cpp
+++ REQ_001/REQ_001.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 
 extern "C" int getPerimeter(int, int);
+extern "C" int getArea(int, int);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,8 +12,20 @@ namespace REQ001
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(PerimeterFunctionality)
 		{
+			// testing perimeter function using length = 2 and width = 3
+			int Result = 0;
+			Result = getPerimeter(2, 3);
+			Assert::AreEqual(10, Result);
+		}
+
+		TEST_METHOD(AreaFunctionality)
+		{
+			// testing area function using length = 4 and width = 5
+			int Result = 0;
+			Result = getArea(4, 5);
+			Assert::AreEqual(20, Result);
 		}
 	};
 }
