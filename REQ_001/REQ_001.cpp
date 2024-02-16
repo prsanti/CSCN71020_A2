3bc334e85e00c884559d9ec94ddefce71f290471
Paul Santiago <70616143+prsanti@users.noreply.github.com>
2024-02-15 22:28:44 -0500

    create setLength test methods

--- REQ_001/REQ_001.cpp
+++ REQ_001/REQ_001.cpp
@@ -3,6 +3,8 @@
 
 extern "C" int getPerimeter(int, int);
 extern "C" int getArea(int, int);
+extern "C" int setLength(int, int);
+extern "C" int setWidth(int, int);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,20 +14,51 @@ namespace REQ001
 	{
 	public:
 		
+		// testing perimeter function
+		// length = 2 and width = 3
 		TEST_METHOD(PerimeterFunctionality)
 		{
-			// testing perimeter function using length = 2 and width = 3
+			
 			int Result = 0;
 			Result = getPerimeter(2, 3);
 			Assert::AreEqual(10, Result);
 		}
 
+		// testing area function
+		// length = 4 and width = 5
 		TEST_METHOD(AreaFunctionality)
 		{
-			// testing area function using length = 4 and width = 5
 			int Result = 0;
 			Result = getArea(4, 5);
 			Assert::AreEqual(20, Result);
+		}
+
+		// testing setLength function
+		// positive value: length = 7
+		TEST_METHOD(SetLengthFunctionality)
+		{
+			int Expected = 7;
+			int Result = 0;
+			setLength(Expected, Result);
+			Assert::AreEqual(Expected, Result);
+		}
+
+		// value 0: length = 0
+		TEST_METHOD(SetLengthLowerBoundFunctionality)
+		{
+			int Expected = 1;
+			int Result = 0;
+			Result = setLength(0, Result);
+			Assert::AreEqual(Expected, Result);
+		}
+
+		// value 100: length = 100
+		TEST_METHOD(SetLengthUpperBoundFunctionality)
+		{
+			int Expected = 1;
+			int Result = 0;
+			Result = setLength(100, Result);
+			Assert::AreEqual(Expected, Result);
 		}
 	};
 }
